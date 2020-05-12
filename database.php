79eb32d2f90b4b1e7e18f41f6c8ebc929e3e1067
12sany <62212507+12sany@users.noreply.github.com>
2020-05-12 18:07:29 +0300

    Update database.php

--- database.php
+++ database.php
@@ -1,11 +1,6 @@
 <?php
-
 $link = mysqli_connect('localhost', 'phpmyadmin', '23121973', 'database');
-
-if (!$link) {
-
-die("Connection failed: " . mysqli_connect_error());
-
-}
-
+    if (!$link) {
+          die("Connection failed: " . mysqli_connect_error());
+    }
 ?>
